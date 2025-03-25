e3211e4b11f8fa796ba3eb322f911dcd439a3ce0
Hectordev4 <hector.valverde.cana@gmail.com>
2025-03-25 14:05:20 +0100

    update AirportController and application.properties for H2 database configuration and JPA settings

--- airportBack/src/main/java/com/cifo/airport/controller/AirportController.java
+++ airportBack/src/main/java/com/cifo/airport/controller/AirportController.java
@@ -2,12 +2,12 @@ package com.cifo.airport.controller;
 
 
 import com.cifo.airport.model.Airport;
-import com.cifo.airport.projection.AirportSummary;
+
 import com.cifo.airport.service.AirportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
